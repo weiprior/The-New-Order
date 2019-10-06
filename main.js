28245bdbe7066cc27899ab8fc2d904a210e7db16
Wei Prior <41879827+weiprior@users.noreply.github.com>
2019-10-06 18:14:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var scene = document.querySelector('a-scene');
+    var splash = document.querySelector('#splash');
+    scene.addEventListener('loaded', function (e) {
+        splash.style.display = 'none';
+    });
+});
+
